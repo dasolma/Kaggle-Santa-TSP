9b903d3ad35a257b118eee19525e4af2f653f04e
David <dasolma@gmail.com>
2015-03-27 11:45:54 +0100

    horizontal greddy zoom

--- santa.r
+++ santa.r
@@ -853,8 +853,9 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
 }
 
 minimun_sol <- function() {
-  sapply(1:150000, function(x) { 
+  sum(sapply(1:150000, function(x) { 
     d = getdistancesto(1:150000, x)  
     min(d[d!=0])
-    }) 
+
+    }))
 }
